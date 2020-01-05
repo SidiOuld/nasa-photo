ba7585bfadd03ff924462997e1d84b0474b790a3
Sidi Ould <sidiould1@gmail.com>
2020-01-04 21:41:47 -0600

    added date input

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "./App.css";
 import Display from "./components/Display";
+// import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Nasa photo of the day 🚀!
-      </p>
-      <Display/>
+      <p>Nasa photo of the day 🚀!</p>
+      <Display />
     </div>
   );
 }
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,29 +1,60 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
-const axios = require('axios');
+const axios = require("axios");
 
+const Container = styled.div``;
 
-function Display(props){
-    const [dataState, setDataState] = useState("Data");
+function Display(props) {
+  const [dataState, setDataState] = useState("Data");
+  const [date, setDate] = useState({ date: "2012-03-14" });
 
-    useEffect(() =>{
-        axios
-        .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14")
-        .then((response) => {
-            console.log("data:", response.data);
-            const data = response.data;
-            setDataState(data.url);
-        })
-    }, [])
+  useEffect(() => {
+    axios
+      .get(
+        // "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14"
+        `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date.date}`
+      )
+      .then(response => {
+        console.log("data:", response.data);
+        const data = response.data;
+        setDataState(data);
+      });
+  }, [date]);
 
-    
-    return (
+  function handleChange(e) {
+    const { name, value } = e.target;
+    console.log(e.target.name);
+    console.log(e.target.value);
+    console.log(date);
 
-        <div>
-            <img className="photoNasa" src={dataState}></img>
-        </div>
-    );
+    setDate({
+      ...date,
+      [name]: value
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <Container>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="date"
+          name="date"
+          value={date.date}
+          onChange={handleChange}
+        />
+      </form>
+
+      <div>
+        <h2>{dataState.title}</h2>
+        <img className="photoNasa" src={dataState.url}></img>
+      </div>
+    </Container>
+  );
 }
 
 export default Display;
-
